575e85e74a4b086351a0f524a2f715464596b268
Shushan <shuhsan@Shushans-MacBook.local>
2022-05-21 23:08:10 +0400

    First commit

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("պիբիպ");
+    int a = 17;
+    int b = 22;
+    int c = a+b;
+        System.out.println(c);
+    }
+}
